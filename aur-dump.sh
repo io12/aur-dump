3afb57d55b0ebd9a14059c5942ce184decdedf91
Benjamin Levy <blevy@protonmail.com>
2019-08-01 11:43:05 -0400

    Add cloning of repo packages

--- aur-dump.sh
+++ aur-dump.sh
@@ -3,7 +3,13 @@
 echo "Fetching list..."
 pkgs=$(curl https://aur.archlinux.org/packages.gz | gunzip | tail -n +2)
 
+echo "Creating dump directory..."
+mkdir dump
+
+echo "Cloning repositories..."
 for pkg in $pkgs
 do
-    echo "$pkg"
+    git clone --depth 1 "https://aur.archlinux.org/$pkg.git" "dump/$pkg"
 done
+
+echo "Done"
